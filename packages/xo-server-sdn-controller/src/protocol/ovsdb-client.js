707c5f9b0105572fe8905d1e2f19d43e35fa8159
BenjiReis <benjamin.reis@vates.fr>
2020-04-23 10:56:29 +0200

    support VM start/stop/migrate/reboot and vif plug/unplug

--- packages/xo-server-sdn-controller/src/protocol/ovsdb-client.js
+++ packages/xo-server-sdn-controller/src/protocol/ovsdb-client.js
@@ -327,10 +327,7 @@ export class OvsdbClient {
       })
     } else {
       this._controller = jsonObjects[0].result[0].uuid[1]
-      log.info('Controller set', {
-        controller: this._controllerUuid,
-        host: this.host.name_label,
-      })
+      log.info('Controller set', { host: this.host.name_label })
     }
 
     socket.destroy()
@@ -402,9 +399,10 @@ export class OvsdbClient {
     const socket = await this._connect()
     const selectResult = await this._select(
       'Interface',
-      ['ofport'],
+      ['name', 'ofport'],
       where,
-      socket
+      socket,
+      false
     )
     if (selectResult === undefined) {
       log.error('No of port found for VIF', {
@@ -416,8 +414,17 @@ export class OvsdbClient {
       return
     }
 
+    let ofport
+    for (const i in selectResult) {
+      const row = selectResult[i]
+      if (!row.name.includes('tap')) {
+        ofport = row.ofport
+        break
+      }
+    }
+
     socket.destroy()
-    return selectResult.ofport
+    return ofport
   }
 
   // ===========================================================================
@@ -567,7 +574,7 @@ export class OvsdbClient {
 
   // ---------------------------------------------------------------------------
 
-  async _select(table, columns, where, socket) {
+  async _select(table, columns, where, socket, onlyOne = true) {
     const selectOperation = {
       op: 'select',
       table,
@@ -601,6 +608,10 @@ export class OvsdbClient {
         host: this.host.name_label,
       })
       return
+    }
+
+    if (!onlyOne) {
+      return jsonResult.rows
     }
 
     // For now all select operations should return only 1 row
